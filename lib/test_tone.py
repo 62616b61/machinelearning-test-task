56d32b71041c84b22bcfcdde28830c137aad4d20
Alex Katsero <alexanderkatsero@gmail.com>
2022-07-03 22:25:17 +0300

    test: add tests for message tone

---
+++ lib/test_tone.py
@@ -0,0 +1,28 @@
+import json
+import logging
+import tempfile
+import unittest
+import pytest
+
+from tone import message_tone
+
+class MessageToneTest(unittest.TestCase):
+  def test_tone_positive(self):
+    tone = message_tone('even')
+
+    self.assertEqual(tone, 'positive')
+
+  def test_tone_negative(self):
+    tone = message_tone('odd')
+
+    self.assertEqual(tone, 'negative')
+
+  def test_tone_unknown(self):
+    tone = message_tone('')
+
+    self.assertEqual(tone, 'unknown')
+
+
+if __name__ == '__main__':
+  logging.getLogger().setLevel(logging.INFO)
+  unittest.main()
